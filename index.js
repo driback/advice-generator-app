f5328ee36fa617c1b322c5e109688f591cab78ea
driback <driback@outlook.com>
2022-08-13 01:14:39 +0700

    lah

---
+++ index.js
@@ -0,0 +1,27 @@
+const API_LINK = "https://api.adviceslip.com/advice";
+const adviceId = document.querySelector(".hero__title");
+const adiceContent = document.querySelector(".hero__advice");
+const adviceCta = document.querySelector(".cta");
+
+const fetchNewAdvice = async () => {
+  const response = await fetch(API_LINK);
+  const adivce = await response.json();
+  return adivce;
+};
+
+const renderAdvice = (adviceObj) => {
+  const { id, advice } = adviceObj;
+  adviceId.textContent = `adivice #${id}`;
+  adiceContent.textContent = advice;
+};
+
+const generateNewAdvice = async () => {
+  const data = await fetchNewAdvice();
+  const advice = data.slip;
+
+  renderAdvice(advice);
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  adviceCta.addEventListener("click", generateNewAdvice);
+});
